80f297aab3dfbb2feebd37ecf0f217bec4f63ffe
BenjaminBTN <alekseykufu@yandex.ru>
2023-05-21 10:25:50 +0300

    added post page

--- src/main/java/ru/galkin/blog/models/Post.java
+++ src/main/java/ru/galkin/blog/models/Post.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String title, anons, full_text;
     private int views;
@@ -52,5 +52,14 @@ public class Post {
 
     public void setViews(int views) {
         this.views = views;
+    }
+
+    public Post() {
+    }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
     }
 }
